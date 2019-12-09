2c630698627560a16f1c053918123cab2753c4f9
Eugene Ji <yj2568@columbia.edu>
2019-12-09 05:42:02 +0000

    add stats to sightings/

--- project/squirrel/urls.py
+++ project/squirrel/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
         path('map/', views.map, name='map'),
         path('sightings/', views.sightings, name='sightings'),
+        path('sightings/<unique_squirrel_id>/', views.update, name='update'),
+        path('sightings/stats', views.stats, name='stats'),
 ]
