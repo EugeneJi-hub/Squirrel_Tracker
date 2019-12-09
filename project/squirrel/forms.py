f44711ac90f9b9e428a09d81807775d970e1e81c
Eugene Ji <yj2568@columbia.edu>
2019-12-09 19:25:25 +0000

    resync

--- project/squirrel/forms.py
+++
@@ -1,6 +0,0 @@
-from django.forms import ModelForm
-from .models import squirrel
-class SquirrelForm(ModelForm):
-    class Meta:
-        model = squirrel
-        fields = "__all__"
--- project/squirrel/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-        path('map/', views.map, name='map'),
-        path('sightings/', views.sightings, name='sightings'),
-        path('sightings/<unique_squirrel_id>/', views.update, name='update'),
-        path('sightings/stats', views.stats, name='stats'),
-]
