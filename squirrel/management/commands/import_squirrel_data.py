40d5c22d7784191ca77721555ecd7aba60cee0b2
Eugene Ji <yj2568@columbia.edu>
2019-12-10 00:52:56 +0000

    modified models.py and import, add export

--- squirrel/management/commands/import_squirrel_data.py
+++ squirrel/management/commands/import_squirrel_data.py
@@ -2,27 +2,42 @@ import os
 import csv
 
 from django.core.management.base import BaseCommand, CommandError
+from distutils.util import strtobool
 from squirrel.models import squirrel 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('args', nargs='*')
-
+        parser.add_argument('csv_file')
+	
     def handle(self, *args, **options):
-        path = args[0]
-        new_names = []
-        with open (path) as data:
-            body = csv.reader(data)
-            names = data.readline().strip().split(',')
-            new_names = [x.lower().replace(' ','_').replace('/','_') for x in names]
-            for row in body:
-                 obj = squirrel()
-                 for i, column in enumerate(row):
-                     if column == 'true':
-                         column = True
-                     if column == 'false':
-                         column = False
-                    
-                     setattr (obj, new_names[i], column)
+        with open(options['csv_file']) as fp:
+            reader = csv.DictReader(fp)
+            data = list(reader)
 
-                 obj.save ()
+        for item in data:
+            s = squirrel(
+                Unique_Squirrel_ID=item['Unique Squirrel ID'],
+		Latitude=item['Y'],
+                Longitude=item['X'],
+                Shift=item['Shift'],
+                Date=item['Date'],
+                Age=item['Age'],
+                Primary_Fur_Color=item['Primary Fur Color'],
+                Location=item['Location'],
+                Specific_Location=item['Specific Location'],
+                Running=strtobool(item['Running']),
+                Chasing=strtobool(item['Chasing']),
+                Climbing=strtobool(item['Climbing']),
+                Eating=strtobool(item['Eating']),
+                Foraging=strtobool(item['Foraging']),
+                Other_Activities=item['Other Activities'],
+                Kuks=strtobool(item['Kuks']),
+                Quaas=strtobool(item['Quaas']),
+                Moans=strtobool(item['Moans']),
+                Tail_flags=strtobool(item['Tail flags']),
+                Tail_twitches=strtobool(item['Tail twitches']),
+                Approaches=strtobool(item['Approaches']),
+                Indifferent=strtobool(item['Indifferent']),
+                Runs_from=strtobool(item['Runs from']),
+            )
+            s.save()
